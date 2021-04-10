b0b785ede9481c31ca095ea424ccd1ab276452bb
JunCoi <juncoi94@gmail.com>
2021-04-10 20:34:51 +0700

    update bai tap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//bootstrap
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import '../node_modules/jquery/dist/jquery.slim';
+import '../node_modules/popper.js/dist/popper';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
